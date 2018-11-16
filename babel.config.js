1341d8af6abe0ee89674d89cc892c8fdc91d1098
Justin Kaseman <justinkaseman@live.com>
2018-11-16 15:53:52 -0800

    Initialize Express server to serve static page

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     presets: ['@babel/preset-env', '@babel/preset-flow', '@babel/preset-react'],
     plugins: [
-        ['@babel/plugin-transform-runtime', { corejs: 2 }],
         'babel-plugin-flow-react-proptypes',
     ],
 }
