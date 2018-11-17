3befbcf7abb9425f70b51002cf7295637db4e69e
Justin Kaseman <justinkaseman@live.com>
2018-11-16 20:04:38 -0800

    Initialize webpack and hot reloading

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-    presets: ['@babel/preset-env', '@babel/preset-flow', '@babel/preset-react'],
-    plugins: [
-        'babel-plugin-flow-react-proptypes',
-    ],
+  presets: ['@babel/preset-env', '@babel/preset-flow', '@babel/preset-react'],
+  plugins: ['babel-plugin-flow-react-proptypes'],
 }
